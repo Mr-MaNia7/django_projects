77611d7fa869f0009002a3d33c4bf1d9283179a3
Mr-MaNia7 <abdulkarimgmohammed@gmail.com>
2022-11-25 09:54:38 +0300

    Add advanced configurations in admin site
    Configure list views and organise detail view layout

--- local_library/catalog/admin.py
+++ local_library/catalog/admin.py
@@ -1,9 +1,37 @@
 from django.contrib import admin
-
 from catalog.models import Author, Book, BookInstance, Genre, Language
 
-admin.site.register(Book)
-admin.site.register(BookInstance)
 admin.site.register(Genre)
-admin.site.register(Author)
 admin.site.register(Language)
+
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+class BookInline(admin.TabularInline):
+    model = Book
+    extra = 0
+    exclude = ['genre', 'summary']
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BookInline, ]
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BookInstanceInline, ]
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'imprint', 'book')
+    list_filter = ('status', 'due_back')
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
